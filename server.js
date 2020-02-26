c9f38dc1e46a97eb988479a7d57ed6786b206af3
Garrett Howard <hylander.garrett@gmail.com>
2020-02-26 12:33:58 -0700

    created server.js and configured express server

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require("express");
+
+var PORT = process.env.PORT || 8080;
+
+var app = express();
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var handlebars = require("express-handlebars");
+
+app.engine("handlebars", handlebars({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burger_controller")
+
+app.use(routes);
+
+app.listen(PORT, function () {
+    console.log("Server listening on: http://localhost:" + PORT);
+})
