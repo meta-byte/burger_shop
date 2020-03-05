78fc08cb30f529ac2fa28109768c293ed2738532
Garrett Howard <hylander.garrett@gmail.com>
2020-03-04 18:24:52 -0700

    refactored for readability

--- server.js
+++ server.js
@@ -4,15 +4,17 @@ var PORT = process.env.PORT || 8080;
 
 var app = express();
 
-app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
+//handlebars
 var handlebars = require("express-handlebars");
 
 app.engine("handlebars", handlebars({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+//routes
 var routes = require("./controllers/burger_controller")
 
 app.use(routes);
